a099d616c533f01fdbab01d02eb075edf45f6240
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-03 00:45:49 -0400

    Third Solution for Problem 1.1

--- Chapter1/problem_1.py
+++ Chapter1/problem_1.py
@@ -1,40 +1,60 @@
 
 #--------------Brute Force -------------
-# string = "ab"
-# isUnique = True
+string = "ab"
+isUnique = True
 
-# #Edge Case: check if the string has a whitespace
-# for char in string:
-#     if char == " ": 
-#         string = string.replace(" ", "")
+#Edge Case: check if the string has a whitespace
+for char in string:
+    if char == " ": 
+        string = string.replace(" ", "")
     
 
-# for i in range(len(string)): 
-#     for j in range(i+1, len(string)):
-#         if string[i] == string[j]:
-#             isUnique = False
-#             break
+for i in range(len(string)): 
+    for j in range(i+1, len(string)):
+        if string[i] == string[j]:
+            isUnique = False
+            break
     
-# if isUnique == True: 
-#     print("String is Unique")
-# else: 
-#     print("String is not Unique")
+if isUnique == True: 
+    print("String is Unique")
+else: 
+    print("String is not Unique")
 
 # -------------------------------------------------
 
 #--------------Using Set (Hash Table) -------------
 string = "aBbcd"
-isStringUnique = True
 #Edge Case: check if the string has a whitespace
-for char in string:
-    if char == " ": 
-        string = string.replace(" ", "")
+def isUnique(string):
+    for char in string:
+        if char == " ": 
+            string = string.replace(" ", "")
 
-if len(set(string)) == len(string) : 
-    isStringUnique = True 
-else: 
-    isStringUnique = False
+    if len(set(string)) == len(string) : 
+        return True
+    else: 
+        return False
 
-print(isStringUnique)
+print(isUnique(string))
 
 # -------------------------------------------------
+
+#--------------Using Bool -------------
+string = "abbcd"
+def isUnique(string): 
+    if len(string) > 128: 
+        return False
+    
+    bool_list = [None] * 128 
+
+    for char in string: 
+        val = ord(char)
+        if bool(bool_list[val]):
+            return False
+        bool_list[val] = True
+    
+    return True
+    
+    
+print(isUnique(string))
+
