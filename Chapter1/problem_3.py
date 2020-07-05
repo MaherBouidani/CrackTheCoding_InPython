5d8cb83f28e2c3abf5db6c1b7bf15219d95df65e
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-05 15:45:58 -0400

    Problem1.3 Brute Force Sol

--- Chapter1/problem_3.py
+++ Chapter1/problem_3.py
@@ -1,1 +1,15 @@
 
+# ----------- Brute Force -----------------
+string = ' John Smith '
+def space_replacement(stirng):
+    str_new = ''
+    for index in range(len(string)):
+        if string[index] == ' ':
+           str_new += '20%'
+        else:
+           str_new += string[index]
+    return str_new 
+
+
+print(space_replacement(string))
+
