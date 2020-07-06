fe8c926ba0d27c808c1ce40a9437a7055c3c6614
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-06 02:54:01 -0400

    Adding more exceptions Problem 1.5

--- Chapter1/problem_5.py
+++ Chapter1/problem_5.py
@@ -1,10 +1,10 @@
 # -------------------Brute ----------------
-string1 = "pales"
-string2 = "pal"
+string1 = "apple"
+string2 = "appless"
 def one_way(string1, string2): 
     #Edge Case, Initial Check
-    if abs(len(string1) - len(string2)) > 1: 
-        return False
+    # if abs(len(string1) - len(string2)) > 1: 
+    #     return False
     #----------------
 
     lis_ascii_1 = [0] * 128
@@ -15,20 +15,22 @@ def one_way(string1, string2):
     for letter in string1:
         lis_ascii_1[ord(letter)] += 1
     
+    
     for letter in string2:
         lis_ascii_2[ord(letter)] += 1
 
     obj_zip = zip(lis_ascii_1, lis_ascii_2)
 
     for i, j in obj_zip:
-        difference += [i-j] # or append
+        difference += [abs(i-j)] # or append
     
+    print(difference)
     count = 0
 
     for item in difference:
-        if item == 1:
+        if item != 0:
             count += 1 
-        if count > 1: 
+        if count > 1 or item ==2: 
             return False
     
     return True
