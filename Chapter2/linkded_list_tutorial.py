8f570e9799825da4bcd173ce8632f18b45bb3243
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-09 02:55:15 -0400

    LinkedList Tutorial in Python 2

--- Chapter2/linkded_list_tutorial.py
+++ Chapter2/linkded_list_tutorial.py
@@ -1,16 +1,63 @@
 # Tutorial on How to make a LinkedList in python. We are not using the module LinkedList, rather creating class definition from scratch
 
 class Node:
-  def __init__(self, data, next=None):
+  def __init__(self, data=None, next=None):
     self.data = data
     self.next = next
   
-  def string(self):
-    return str(self)
+  
+
+class Head: 
+    def __init__(self): 
+        self.headPointer = None 
+     
+
+    def print(self):
+        current = self.headPointer
+        while current:
+            print(current.data)
+            current = current.next
+        
+    def insert_node_atEnd(self,data):
+        newNode = Node(data)
+        if(self.headPointer):
+            current = self.headPointer
+            while current.next:
+                current = current.next
+            
+            current.next = newNode
+        else:
+            self.headPointer = newNode
+
+    def list_length(self):
+        count = 0
+        current = self.headPointer
+        while current:
+            count += 1
+            current = current.next
+        
+        return print("list length is:", count)
+    
+
+linked_list_head = Head()
+# first_node = Node(1)
+# second_node = Node(2)
+# third_node = Node(3)
+# first_node.next = second_node
+# second_node.next = third_node
+# linked_list_head.headPointer = first_node
+
+# linked_list_head.insert_node_atEnd(7)
+
+for data in [1,2,3,7]:
+    linked_list_head.insert_node_atEnd(data)
+
+linked_list_head.print()
+linked_list_head.list_length()
 
 
-first_node = Node(1)
-second_node = Node(2)
-third_node = Node(3)
 
-print(first_node.data, first_node.next)
+
+
+
+
