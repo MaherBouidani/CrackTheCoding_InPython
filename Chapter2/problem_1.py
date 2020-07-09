cd29bbda1ff636a1d19d500457804830856e77d6
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-09 16:40:37 -0400

    problem 1 - blue print linked list init

--- Chapter2/problem_1.py
+++ Chapter2/problem_1.py
@@ -1,1 +1,33 @@
 
+class Node: 
+    def __init__(self, data=None, next=Node): 
+        self.data = data
+        self.next = next
+    
+class SingleLinkedList: 
+    def __init__(self): 
+        self.head = None
+
+    def add_node(self, data):
+        newNode = Node(data)
+        
+        if self.head:
+            current = self.head
+            while current.next: 
+                current = current.next
+            current.next = newNode
+
+        else: 
+            self.head = newNode
+            
+    
+ll = SingleLinkedList()
+
+
+for item in [4,15,13,13,16,3]:
+    ll.add_node(item)
+
+
+
+
+
