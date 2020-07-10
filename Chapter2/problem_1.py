91a7be3ce44ce0dbf532a86dc36ee559e7ab981f
Maher  Bouidani <maher.buedani@gmail.com>
2020-07-10 03:27:16 -0400

    Problem 1 Hashtable Solution

--- Chapter2/problem_1.py
+++ Chapter2/problem_1.py
@@ -27,8 +27,8 @@ class SingleLinkedList:
             ll_data +=  [current.data] #Ref_To NOTE1
             current = current.next
         return ll_data
-    
-    def remove_duplicates(self):
+ # -------------------Brute Force ------------------------       
+    def remove_duplicates_brute_sol(self):
         current = self.head 
 
         while current:
@@ -45,6 +45,23 @@ class SingleLinkedList:
                 else:
                     prev_node = self.head
             current = current.next
+ # -------------------Hash Table ------------------------   
+    def remove_duplicates_hashtable_sol(self):
+        hash_table = set()
+        current = self.head
+        prev_node = None
+        while current:
+            if prev_node is not None:
+                if current.data in hash_table:
+                    prev_node.next = current.next
+                    return 
+                prev_node = current
+                hash_table.add(current.data)
+                current = current.next
+            else:
+                prev_node = self.head
+
+
         
             
             
@@ -55,12 +72,12 @@ class SingleLinkedList:
 ll = SingleLinkedList()
 
 
-for item in [13,13,15,8,16,3]:
+for item in [13,34,76,8,16,13]:
     ll.add_node(item)
 
 print(ll.print_ll())
 
-ll.remove_duplicates()
+ll.remove_duplicates_hashtable_sol()
 print(ll.print_ll())
 
 
